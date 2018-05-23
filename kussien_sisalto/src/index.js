aa2e938c9e7b9dc804eec7266313cb1ec6eea70e
Jere Tofferi <jere.tofferi@gmail.com>
2018-05-24 00:53:39 +0300

    tehtava1_2

--- kussien_sisalto/src/index.js
+++ kussien_sisalto/src/index.js
@@ -1,8 +1,49 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+const Kurssi = (props) => {
+  let nimi, osat
+  ({nimi, osat} = props.kurssikuvaus)
+  
+  return(
+    <div>
+      <h1>{nimi}</h1>
+      {osat.map((osa, i) => <p key={i}> {osa.nimi} {osa.tehtavia} </p>)}
+    </div>
+  )
+}
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Kurssi kurssikuvaus={kurssi} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
