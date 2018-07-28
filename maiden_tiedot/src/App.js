74a6679900cdb72dfecfecde46879d0c95e2bc14
Jere Tofferi <jere.tofferi@gmail.com>
2018-07-29 00:57:13 +0300

    Tehtavat 2.12 ja 2.13

--- maiden_tiedot/src/App.js
+++ maiden_tiedot/src/App.js
@@ -1,18 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: [],
+      newSearch: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ newSearch: event.target.value });
+  };
+
+  showCountry = (countryname) => () => {
+    this.setState({ newSearch: countryname})
+  }
+
+  componentDidMount() {
+    axios.get("https://restcountries.eu/rest/v2/all").then(response => {
+      this.setState({ countries: response.data });
+    });
+  }
+
   render() {
+    const search = this.state.newSearch.toLowerCase();
+    const countries = this.state.countries.filter(country =>
+      country.name.toLowerCase().includes(search)
+    );
+
+    let showThoseCountries = [];
+
+    // Show filtered search, three alternatives
+    if (countries.length > 10) {
+      showThoseCountries = "Too many matches, specify another filter.";
+    } else if (countries.length > 1) {
+      const countryList = countries.map(country => (
+        <li key={country.alpha3Code}><div onClick={this.showCountry(country.name)}>{country.name}</div></li>
+      ));
+      showThoseCountries = <ul>{countryList}</ul>;
+    } else {
+      showThoseCountries = countries.map(country => {
+        return (
+          <div>
+            <h2>{country.name}</h2>
+            <p>capital: {country.capital}</p>
+            <p>population: {country.population}</p>
+            <img src={country.flag} alt="flag" style={{ width: "50%" }} />
+          </div>
+        );
+      });
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <label id="searchBox">find countries:</label>
+          <input value={this.state.newSearch} onChange={this.handleChange} />
+        </form>
+        <div>{showThoseCountries}</div>
       </div>
     );
   }
